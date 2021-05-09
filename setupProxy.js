0aca16153771c458c54e4c530fbd6dd9ee71c818
santafg <santanughosh28111998@gmail.com>
2021-05-09 19:38:21 +0530

    dcmit

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://uzer-auth.herokuapp.com/auth/loggedIn',
+      target: 'https://uzer-auth.herokuapp.com',
       changeOrigin: true,
     })
   );
