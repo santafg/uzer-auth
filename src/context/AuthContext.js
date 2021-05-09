230c269275756238d048c3ef6a7546fc72722f10
santafg <santanughosh28111998@gmail.com>
2021-05-09 21:04:42 +0530

    dcmit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,16 +4,16 @@ import axios from "axios";
 const AuthContext = createContext();
 
 function AuthContextProvider(props) {
-  const [loggedIn, setLoggedIn] = useState(undefined);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  async function getLoggedIn() {
-    const loggedInRes = await axios.get("/auth/loggedIn");
-    setLoggedIn(loggedInRes.data);
-  }
+  // async function getLoggedIn() {
+  //   const loggedInRes = await axios.get("/auth/loggedIn");
+  //   setLoggedIn(loggedInRes.data);
+  // }
 
-  useEffect(() => {
-    getLoggedIn();
-  }, []);
+  // useEffect(() => {
+  //   getLoggedIn();
+  // }, []);
   return <AuthContext.Provider value={{loggedIn , getLoggedIn}}>
       {props.children}
   </AuthContext.Provider>;
